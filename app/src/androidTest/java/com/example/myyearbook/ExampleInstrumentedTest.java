d94eb4cbcf65b4dc02fc74fa051d782b7aa9f597
Barrytech <barryabdoul2024@gmail.com>
2021-02-25 20:22:56 -0500

    cleaned the code to have a simple hello world app

--- app/src/androidTest/java/com/example/myyearbook/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myyearbook/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.helloworld;
+package com.example.myyearbook;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.helloworld", appContext.getPackageName());
+        assertEquals("com.example.myyearbook", appContext.getPackageName());
     }
 }
--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.helloworld;
+package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
