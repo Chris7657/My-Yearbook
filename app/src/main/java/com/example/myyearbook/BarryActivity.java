527c677e05c5b4865ce0b6e6efe214f038b84b32
Barrytech <barryabdoul2024@gmail.com>
2021-03-04 00:52:26 -0500

    added my page to project

---
+++ app/src/main/java/com/example/myyearbook/BarryActivity.java
@@ -0,0 +1,14 @@
+package com.example.myyearbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barry);
+    }
+}
