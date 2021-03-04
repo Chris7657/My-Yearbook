0b1c1c01c52e8d56c52ff6add1728d05904b12aa
Barrytech <barryabdoul2024@gmail.com>
2021-03-04 02:04:14 -0500

    added share feature. neeed to debug and make it work

--- app/src/main/java/com/example/myyearbook/BarryActivity.java
+++ app/src/main/java/com/example/myyearbook/BarryActivity.java
@@ -3,12 +3,28 @@ package com.example.myyearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class BarryActivity extends AppCompatActivity {
-
+    Button share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barry);
+        share = (Button)findViewById(R.id.button);
+        sharee.setOnClickListneer(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "The body here";
+                String shareSub = "The subject here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT, shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.creeateChooseer(myIntent, "Share using"));
+            }
+        });
+
+
     }
 }
